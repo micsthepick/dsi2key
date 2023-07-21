6635693a52d2971416d4a13bc6dd92142ab8420e
micsthepick <11528421+micsthepick@users.noreply.github.com>
2023-07-22 08:12:24 +1000

    gitignore, and use correct dir for ds2key.ini

--- client/compile.sh
+++ client/compile.sh
@@ -1,5 +1,5 @@
 make -f Makefile_nds
 echo "copying build to ../out"
-mkdir -p ../out/COPY_CONTENTS_TO_SD_ROOT
-cp ds2key.ini.example ../out/COPY_CONTENTS_TO_SD_ROOT/ds2key.ini
+mkdir -p ../out/COPY_CONTENTS_TO_SD_ROOT/ds2key
+cp ds2key.ini.example ../out/COPY_CONTENTS_TO_SD_ROOT/ds2key/ds2key.ini
 cp ds2key.nds ../out/COPY_CONTENTS_TO_SD_ROOT
